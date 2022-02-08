c9c71e79bf61b08b998091fb96aa41f4381b9b35
VivekanandaBediya <vivekanandbediya006@gmail.com>
2022-02-08 22:56:37 +0530

    javascript commited

---
+++ index.js
@@ -0,0 +1,22 @@
+//variable declaration
+var a;  //int string float?
+
+//variable initilization
+a = 10;
+console.log(typeof a)
+console.log(a)
+
+a = "hello";
+console.log(a); //re initialization
+
+//typeof operator->it tells us the type of data stored in a variable
+console.log(typeof a);
+
+a = true;
+console.log(a);
+console.log(typeof a);
+
+
+a = null;
+console.log(a)
+console.log(typeof a);
